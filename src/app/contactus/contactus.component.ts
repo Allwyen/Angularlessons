c580548cef6341fff64169235d1d116804871eb4
Allwyen <allwyen27@gmail.com>
2019-08-26 16:20:57 +0530

    angular day 1

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -7,6 +7,22 @@ import { NgForm } from '@angular/forms';
 })
 export class ContactusComponent implements OnInit {
 
+  getName='';
+  getEmail='';
+  getMsg='';
+
+  onSubmit(data:NgForm)
+  {
+    this.getName=data.value.name;
+    console.log(data.value.name);
+
+    this.getEmail=data.value.email;
+    console.log(data.value.email);
+    
+    this.getMsg=data.value.msg;
+    console.log(data.value.msg);
+  }
+
   constructor() { }
 
   ngOnInit() {
