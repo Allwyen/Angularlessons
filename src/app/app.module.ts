76ba48b73e358378297f6c8547e9b45a2037f2e9
Allwyen <allwyen27@gmail.com>
2019-08-27 16:13:25 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { Routes,RouterModule } from '@angular/router';
 
 const appRoutes:Routes=[
   {path:'',component:ContactusComponent},
-  {path:'view',component:ViewComponent}
+  {path:'view',component:ViewComponent},
+  {path:'search',component:SearchComponent}
 ]
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { ViewComponent } from './view/view.component';
+import { SearchComponent } from './search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContactusComponent,
-    ViewComponent
+    ViewComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
